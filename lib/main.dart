d3ece10cdf9fdded9dfb4bf08f0f5529d1b2ceec
Marco Silva <149703754+marco19silvaaa@users.noreply.github.com>
2024-07-07 17:36:14 +0100

    pequena alteração

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,9 @@ import 'package:projeto_ispgaya/pages/user_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   FirebaseFirestore.instance.settings = const Settings(
     persistenceEnabled: true,
   );
