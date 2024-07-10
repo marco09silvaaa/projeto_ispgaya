8748454f7435d7ac47bac953f9c4bb006d0ab7d7
Marco Silva <149703754+marco19silvaaa@users.noreply.github.com>
2024-07-10 10:14:06 +0100

    Trabalho 90% completo
    Apenas precisa de alguns ajustes finais.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:projeto_ispgaya/components/navbar.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:intl/intl.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Appointment> _todayAppointments = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchTodayAppointments();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(50, 255, 160, 0),
       appBar: AppBar(
         title: const Text('Home Page'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 100,
       ),
-      body: const Center(
-        child: Text(
-          'Conteúdo da Home Page',
-          style: TextStyle(fontSize: 24.0),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset(
+                  'images/logo_ispgaya.png',
+                  height: 100,
+                ),
+              ),
+              const SizedBox(height: 60),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Bem-vindo de volta Marco!',
+                      style: TextStyle(
+                          fontSize: 32.0, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Tarefas de hoje:',
+                    style:
+                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              _buildAppointmentList(),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: CustomNavBar(
@@ -36,5 +96,69 @@ class HomePage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  Widget _buildAppointmentList() {
+    if (_todayAppointments.isEmpty) {
+      return const Center(child: Text('Nenhuma tarefa para hoje'));
+    }
+
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: _todayAppointments.length,
+      itemBuilder: (context, index) {
+        final appointment = _todayAppointments[index];
+        final DateFormat timeFormat = DateFormat('HH:mm');
+        final String startTime = timeFormat.format(appointment.startTime);
+        final String endTime = timeFormat.format(appointment.endTime);
+        return ListTile(
+          title: Text(appointment.subject),
+          subtitle: Text(
+            '$startTime - $endTime\nSala: ${appointment.notes}',
+          ),
+          leading: CircleAvatar(
+            backgroundColor: appointment.color,
+          ),
+        );
+      },
+    );
+  }
+
+  void _fetchTodayAppointments() async {
+    CollectionReference events =
+        FirebaseFirestore.instance.collection('events');
+    QuerySnapshot snapshot = await events.get();
+    List<Appointment> appointments = [];
+
+    DateTime now = DateTime.now();
+    DateTime startOfDay = DateTime(now.year, now.month, now.day);
+    DateTime endOfDay = DateTime(now.year, now.month, now.day, 23, 59, 59);
+
+    for (var doc in snapshot.docs) {
+      Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+      DateTime startTime = (data['startTime'] as Timestamp).toDate();
+      DateTime endTime = (data['endTime'] as Timestamp).toDate();
+      String subject = data['subject'];
+      String room = data['room'] ?? '';
+      Color color = Color(int.parse(data['color']));
+
+      if (startTime.isAfter(startOfDay) && endTime.isBefore(endOfDay)) {
+        appointments.add(
+          Appointment(
+            startTime: startTime,
+            endTime: endTime,
+            subject: subject,
+            color: color,
+            notes: room,
+          ),
+        );
+      }
+    }
+
+    setState(() {
+      _todayAppointments.addAll(appointments);
+    });
   }
 }
--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,19 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_ispgaya/components/navbar.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class UserPage extends StatelessWidget {
+class UserPage extends StatefulWidget {
   const UserPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  File? _profileImage;
+  final TextEditingController _nameController = TextEditingController();
+  bool _notificationsEnabled = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(50, 255, 160, 0),
       appBar: AppBar(
         title: const Text('User Page'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 100,
       ),
-      body: const Center(
-        child: Text(
-          'Conteúdo da User Page',
-          style: TextStyle(fontSize: 24.0),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          child: Column(
+            children: [
+              Center(
+                child: Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundImage: _profileImage != null
+                          ? FileImage(_profileImage!)
+                          : const AssetImage('images/default_profile.png')
+                              as ImageProvider,
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: GestureDetector(
+                        onTap: _pickImage,
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: const BoxDecoration(
+                            color: Colors.orange,
+                            shape: BoxShape.circle,
+                          ),
+                          child: const Icon(
+                            Icons.camera_alt,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              _buildNameField(),
+              const SizedBox(height: 20),
+              _buildNotificationsSwitch(),
+              const SizedBox(height: 50),
+              _buildSaveButton(),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: CustomNavBar(
@@ -36,5 +92,95 @@ class UserPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  Widget _buildNameField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Nome',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 8),
+        TextField(
+          controller: _nameController,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: const Color(0xffF7F7F9),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(14),
+              borderSide: BorderSide.none,
+            ),
+            hintText: 'Marco Silva',
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildNotificationsSwitch() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          'Notificações',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Switch(
+          value: _notificationsEnabled,
+          onChanged: (value) {
+            setState(() {
+              _notificationsEnabled = value;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSaveButton() {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color.fromARGB(255, 255, 160, 0),
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+        textStyle: const TextStyle(fontSize: 18),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(14),
+        ),
+        elevation: 0,
+      ),
+      onPressed: () {
+        // Lógica para salvar as informações do usuário
+        //String name = _nameController.text;
+        //bool notifications = _notificationsEnabled;
+
+        // Implementar a lógica de salvamento aqui, por exemplo, enviar para um servidor ou salvar localmente
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Informações salvas com sucesso!'),
+          ),
+        );
+      },
+      child: const Text('Salvar'),
+    );
+  }
+
+  Future<void> _pickImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _profileImage = File(pickedFile.path);
+      });
+    }
   }
 }
